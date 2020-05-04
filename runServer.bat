e8e14b5c50fa4ff0bb0c4b7fdad655307fb1496c
Pawel <14218944+pwodyk@users.noreply.github.com>
2020-05-04 18:44:49 +0100

    added basic script to launch the mysql server and website server for testing

---
+++ runServer.bat
@@ -0,0 +1,5 @@
+net start MYSQL80
+
+cd /D F:\OneDrive\Code\ITB\WebApps\Project\Git
+php -S 127.0.0.1:5050 -t public_html
+pause
---
+++ runWithNewData.bat
@@ -0,0 +1,7 @@
+net start MYSQL80
+
+php db\migrateAndLoadFixtures.php
+
+cd /D F:\OneDrive\Code\ITB\WebApps\Project\Git
+php -S 127.0.0.1:5050 -t public_html
+pause
